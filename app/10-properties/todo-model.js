b5c79cd30d4917bbdb7d3ecd0fbabd99008f23ac
dalerlarsen <dalerlarsen@gmail.com>
2016-01-24 20:26:01 -0500

    10-properties

---
+++ app/10-properties/todo-model.js
@@ -0,0 +1,24 @@
+System.register([], function(exports_1) {
+    "use strict";
+    var TodoModel;
+    return {
+        setters:[],
+        execute: function() {
+            TodoModel = (function () {
+                function TodoModel(title) {
+                    if (title === void 0) { title = ""; }
+                    this.title = title;
+                    this.status = "started";
+                }
+                ;
+                TodoModel.prototype.toggle = function () {
+                    this.status =
+                        this.status == "started" ? "completed" : "started";
+                };
+                return TodoModel;
+            }());
+            exports_1("TodoModel", TodoModel);
+        }
+    }
+});
+//# sourceMappingURL=todo-model.js.map
