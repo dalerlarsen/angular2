7f19c09b7e50e8de0d89358ff620522785d54539
dalerlarsen <dalerlarsen@gmail.com>
2016-01-24 19:51:45 -0500

    08-ng-model

---
+++ app/08-ng-model/todo-input.ts
@@ -0,0 +1,21 @@
+  import {Component, Inject} from "angular2/core";
+  import {TodoService} from "./todo-service";
+
+  @Component({
+    selector: 'todo-input',
+    template: `<div>
+    <form (submit)="onSubmit()">
+    <input type="text" [(ngModel)]="todoModel" >
+    </form>
+    </div>`
+  })
+  export class TodoInput{
+    todoModel;
+    constructor(@Inject(TodoService) public todoService) {
+
+    }
+    onSubmit() {
+      this.todoService.todos.push(this.todoModel);
+      console.log(this.todoService.todos);
+    }
+  };
