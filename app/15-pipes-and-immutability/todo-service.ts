78150ffe8e7597b54a4fd11c40d9536315566969
dalerlarsen <dalerlarsen@gmail.com>
2016-01-24 21:38:52 -0500

    15-pipes-and-immutability

---
+++ app/15-pipes-and-immutability/todo-service.ts
@@ -0,0 +1,14 @@
+import {Injectable} from "angular2/core"
+import {TodoModel} from "./todo-model";
+@Injectable()
+export class TodoService {
+  todos = [
+    new TodoModel("eat"),
+    new TodoModel("sleep"),
+    new TodoModel("code")
+  ];
+
+  addTodo(todo: TodoModel) {
+    this.todos = [...this.todos, todo];
+  }
+}
