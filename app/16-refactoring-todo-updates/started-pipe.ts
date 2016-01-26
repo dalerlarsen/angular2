7e782f49587158300e94e5c893df1092ef8fa4e2
dalerlarsen <dalerlarsen@gmail.com>
2016-01-26 12:17:03 -0500

    16-refactoring-todo-updates

---
+++ app/16-refactoring-todo-updates/started-pipe.ts
@@ -0,0 +1,12 @@
+import {Pipe} from "angular2/core";
+
+@Pipe({
+  name: "started"
+})
+
+export class StartedPipe{
+  transform(value) {
+    return value.filter((item)=>item.status === "started");
+    // return value;
+  }
+}
