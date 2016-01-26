09915ca630791ee041552d1ae7f3ee32f37e7a36
dalerlarsen <dalerlarsen@gmail.com>
2016-01-26 12:36:04 -0500

    17-select-and-ng-model

---
+++ app/17-select-and-ng-model/main.ts
@@ -0,0 +1,21 @@
+import {bootstrap} from "angular2/platform/browser";
+import {Component, provide} from "angular2/core";
+import {TodoInput} from "./todo-input";
+import {TodoService} from "./todo-service";
+import {TodoList} from "./todo-list";
+import {StatusSelector} from "./status-selector";
+
+@Component({
+  selector: 'app',
+  directives: [TodoInput, TodoList, StatusSelector],
+  template: `<div>
+  <todo-input></todo-input>
+  <status-selector (select)="status = $event"></status-selector>
+  <todo-list [status]="status"></todo-list>
+  </div>`
+})
+
+class App{}
+
+// bootstrap(App, [provide('whatever', {useClass:TodoService})]);
+bootstrap(App, [TodoService]);
---
+++ app/17-select-and-ng-model/search-pipe.ts
@@ -0,0 +1,12 @@
+import {Pipe} from "angular2/core";
+
+@Pipe({
+  name: "search"
+})
+
+export class SearchPipe{
+  transform(value) {
+    return value.filter((item)=>item.title.startsWith('s'));
+    // return value;
+  }
+}
